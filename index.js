a64bf458e4ddaa0d92000c79ac8f4e4b78964cbb
Mari-bekker <mshooch@gmail.com>
2022-02-27 19:36:11 -0500

    add orchestrating function

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
-const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes } = require('./iss');
+const { fetchMyIP, fetchCoordsByIP, fetchISSFlyOverTimes, nextISSTimesForMyLocation } = require('./iss');
 
-fetchMyIP((error, ip) => {
+nextISSTimesForMyLocation((error, passTimes) => {
   if (error) {
-    console.log("It didn't work!", error);
-    return;
+    return console.log("It didn't work!", error);
   }
+  // if success print out the details
+  printPassTimes(passTimes);
+})
 
-  console.log("It worked! the returned IP:" , ip);
-});
+const printPassTimes = function(passTimes) {
+  for (const pass of passTimes) {
+    const datetime = new Date(0);
+    datetime.setUTCSeconds(pass.risetime);
+    const duration = pass.duration;
+    console.log(`Next pass at ${datetime} for ${duration} seconds`);
+  }
+}
+
+// fetchMyIP((error, ip) => {
+//   if (error) {
+//     console.log("It didn't work!", error);
+//     return;
+//   }
+
+//   console.log("It worked! the returned IP:" , ip);
+// });
 
 // fetchCoordsByIP('135.23.97.243', (error, coordinates) => {
 //   if (error) {
